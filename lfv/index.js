95407c75bfe5b313bcfc3eca1a9c70f752cafe1a
maxvankampen <max.van.kampen@alicon.nl>
2020-11-05 14:41:16 +0100

    0.0.0

--- lfv/index.js
+++
@@ -1,7 +0,0 @@
-Aim = require('aim-control');
-// Aim.operations.setServo = function() {
-//     console.log('TEST');
-// };
-// Aim.setServo = function() {
-//     console.log('TEST');
-// };
--- lfv/lfv_Verkeerslichten_Verkeersbuis/lfv_Verkeerslichten_Verkeersbuis.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:node
-cls
-node lfv_Verkeerslichten --config=config_Verkeerslichten.json
-pause
-goto node
