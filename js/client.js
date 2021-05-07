02f9b8037e734f88893fdb6c18dd3f3e6746e730
Max van Kampen <max.van.kampen@alicon.nl>
2021-05-07 15:41:49 +0200

    0.0.0

--- js/client.js
+++ js/client.js
@@ -1,15 +1,17 @@
-document.head.createElement('link', {rel:"stylesheet", href:"/sites/aliconnect/api/css/client.css"});
-(function() {
-  AIM.extend({
-    web:{
-      site:{
-        index:[
-          [
-            ['H1', '', 'Welkom op Dynniq'],
-            ['P', '', ``],
-          ],
-        ]
-      }
-    }
-  });
-})();
+console.log('DYNNIQ');
+
+// document.head.createElement('link', {rel:"stylesheet", href:"/sites/aliconnect/api/css/client.css"});
+// (function() {
+//   AIM.extend({
+//     web:{
+//       site:{
+//         index:[
+//           [
+//             ['H1', '', 'Welkom op Dynniq'],
+//             ['P', '', ``],
+//           ],
+//         ]
+//       }
+//     }
+//   });
+// })();
