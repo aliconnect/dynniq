3ad619c6e193c8d8475c1c8371bbc81f3d62da4e
Max van Kampen <max.van.kampen@alicon.nl>
2021-05-21 10:17:27 +0200

    Auto commit

---
+++ js/config.js
@@ -0,0 +1,13 @@
+$().config({
+  client_id: "48569-da6d0ebb-942e-4c5c-a18f-ab7e457a61f8",
+  info: {
+    title: "Dynniq",
+  },
+  client: {
+    servers: [
+      {
+        url: "https://dynniq.aliconnect.nl/api"
+      }
+    ]
+  },
+});
